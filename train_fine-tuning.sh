2b1953b3dd57e6c481bd87e3bb96b135f59da458
yanbc <imyanbc@gmail.com>
2020-05-19 14:59:45 +0800

    Remove unused files

--- train_fine-tuning.sh
+++
@@ -1,1 +0,0 @@
-/opt/caffe/build/tools/caffe train -solver /hand_detect/models/VGGNet/SSD_300x300/solver.prototxt -weights /hand_detect/models/VGGNet/SSD_300x300/VGG_SSD_300x300_iter_20633.caffemodel
